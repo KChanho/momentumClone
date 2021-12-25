3263fdf81ff1bed3556f8bf87177f3eb13aaa99f
KChanho <kch10406@gmail.com>
2021-12-25 15:50:08 +0900

    color of the year

--- js/background.js
+++ js/background.js
@@ -1,10 +1,51 @@
-//https://korean.visitkorea.or.kr/ "이번달 인기 검색 여행지는 어디?"
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.png", "5.jpeg", "6.jpg", "7.jpg", "8.bmp", "9.jpg", "10.jpg", "11.jpg"];
+//Pantone "Color of the year" {color, year, name}
+const colors = [
+{   color: "#6768AB",
+    year: 2022,
+    name: "Very Peri"
+}, 
+{   color: "#939597",
+    year: 2021,
+    name: "Ultimate Gray"
+},
+{   color: "#F5DF4E",
+    year: 2021,
+    name: "Illuminating" 
+},
+{   color: "#0F4C82",
+    year: 2020,
+    name: "Classic Blue"
+},
+{   color: "#939597",
+    year: 2019,
+    name: "Living Coral"
+},
+{   color: "#675094",
+    year: 2018,
+    name: "Ultra Violet"
+},
+{   color: "#85AF4B",
+    year: 2017,
+    name: "Greenery"
+},
+{   color: "#F6CAC9",
+    year: 2016,
+    name: "Rose Quartz"
+},
+{   color: "#90A7D1",
+    year: 2016,
+    name: "Serenity"
+},
+{   color: "#985051",
+    year: 2015,
+    name: "Marsala"
+},
+];
 
-const chosenimage = images[Math.floor(Math.random() * images.length)];
+const name = document.querySelector("#quote span:first-child");
+const year = document.querySelector("#quote span:last-child");
+const chosenColor = colors[Math.floor(Math.random() * colors.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenimage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundColor = chosenColor.color;
+name.innerText = chosenColor.name;
+year.innerText = chosenColor.year;
